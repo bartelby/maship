d98300585a976a763afdf25243082ddd5933a083
Peter Sisk <bartelby@gmail.com>
2019-01-23 02:32:30 +0000

    initial commit

---
+++ api/models.py
@@ -0,0 +1,71 @@
+"""
+    The Object-relational layer, including geospatial information
+"""
+from django.contrib.gis.db import models
+
+UNIT_SRID = 4326
+
+class Driver(models.Model):
+    """
+        Driver consist only of a driver ID and geographical coordinates.
+    """
+    driverId = models.IntegerField(primary_key=True)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    point = models.PointField(srid=UNIT_SRID)
+    objects = models.GeoManager()
+
+    class Meta:
+        """ the postgres db table """
+        db_table = 'driver'
+
+    def __str__(self):
+        return '%s %s %s' % (self.driverId, self.lat, self.lon)
+
+class Shipment(models.Model):
+    """
+        Shipment consists of an ID, the shipment's geographical coordinates
+        and a pointer to the driver that accepts the shipment.
+    """
+    shipmentId = models.BigIntegerField(primary_key=True)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    point = models.PointField(srid=UNIT_SRID)
+    driver_accepted = models.ForeignKey('Driver',
+                                        models.DO_NOTHING,
+                                        db_column='driver',
+                                        null=True)
+    objects = models.GeoManager()
+    
+    class Meta:
+        """ the postgres db table """
+        db_table = 'shipment'
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.shipmentId,
+                                self.lat,
+                                self.lon,
+                                self.driver_accepted if self.driver_accepted else '')
+
+class Dispatch(models.Model):
+    """
+        Tracks which shipments have been dispatched to which drivers
+    """
+    id = models.AutoField(primary_key=True)
+    shipment = models.ForeignKey('Shipment',
+                                 models.DO_NOTHING,
+                                 db_column='dispatch_shipment')
+    driver = models.ForeignKey('Driver',
+                               models.DO_NOTHING,
+                               db_column='dispatch_driver')
+    accepted = models.BooleanField(null=True)
+
+    class Meta:
+        """ the postgres db table """
+        db_table = 'dispatch'
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.id,
+                                self.shipment,
+                                self.driver,
+                                bool(self.accepted))
---
+++ etl/import_data.py
@@ -0,0 +1,63 @@
+"""
+    Imports driver and shipment data from the json formatted files drivers.json and
+    shipments.json and inserts the data into the driver and shipment tables in the
+    geospatial dataabase maship. The files live in ./data directory.
+"""
+import os
+from json import loads as json_loads
+import django
+from django.contrib.gis.geos import Point
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maship.settings')
+django.setup()
+from api.models import Driver, Shipment
+
+def import_data():
+    """
+        Reads json files from ./data directoruy and imports entries
+        into Driver and Shipment tables in the database.
+    """
+    driver_filename = './data/drivers.json'
+    shipment_filename = './data/shipments.json'
+    # Import drivers
+    data = ''
+    with open(driver_filename) as _f:
+        data = _f.read()
+    if data:
+        json_data = json_loads(data)
+        for driver_id, loc_data in json_data.items():
+            coordinates = loc_data['coordinates']
+            lat = coordinates['latitude']
+            lon = coordinates['longitude']
+            #Create and save the Driver object
+            kwargs = {
+                'driverId': driver_id,
+                'lat': lat,
+                'lon': lon,
+                'point': Point(lon, lat)
+            }
+            Driver(**kwargs).save()
+            print('saved driver {0}, {1} {2}'.format(driver_id, lat, lon))
+
+    # Import shipments
+    data = ''
+    with open(shipment_filename) as _f:
+        data = _f.read()
+    if data:
+        json_data = json_loads(data)
+        for shipment_id, ship_data in json_data.items():
+            coordinates = ship_data['coordinates']
+            lat = coordinates['latitude']
+            lon = coordinates['longitude']
+            #Create and save Shipment object
+            kwargs = {
+                'shipmentId': shipment_id,
+                'lat': lat,
+                'lon': lon,
+                'point': Point(lon, lat)
+            }
+            Shipment(**kwargs).save()
+            print("saved shipment {0} {1} {2}".format(shipment_id, lat, lon))
+
+if __name__ == "__main__":
+    import_data()
